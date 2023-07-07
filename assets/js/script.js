59129556413c9929c4a3c7c2d425649b795bde17
hideki watabe <hideki.w.1971@gmail.com>
2023-07-08 04:25:42 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  // setting
+  $(function () {
+    $(window).on("scroll touchmove", function () {
+      //スクロール中に判断する
+      $(".kore").stop(); //アニメーションしている場合、アニメーションを強制停止
+      $(".kore").css('display', 'none').delay(500).fadeIn('fast');
+      //スクロール中は非表示にして、500ミリ秒遅らせて再び表示
+    });
+  });
+  // /setting
+});
